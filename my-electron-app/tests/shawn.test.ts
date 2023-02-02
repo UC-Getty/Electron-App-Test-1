ccf5aef4a444a8651c3aba8037006c3503fde60c
t-so <104852414+t-so@users.noreply.github.com>
2023-02-01 20:12:07 -0700

    test.fail()

--- my-electron-app/tests/shawn.test.ts
+++ my-electron-app/tests/shawn.test.ts
@@ -18,87 +18,95 @@ test.beforeAll(async () =>{
     expect(await page.screenshot()).toMatchSnapshot('firstWindow.png');
 })
 
+test.afterAll(async () => {
+    await electronApp.close();
+});
+
 
 test.describe('test sequence', () => {
 
-test("login", async () =>{
+    test("login", async () =>{
 
-    //Enter username and password and click login
-    await page.fill("//*[@id='username']","test")
-    await page.fill("//*[@id='password']","12345678")
-    await page.click("//*[@id='login']")
+        //Enter username and password and click login
+        await page.fill("//*[@id='username']","test")
+        await page.fill("//*[@id='password']","12345678")
+        await page.click("//*[@id='login']")
 
-    //waits for the element with the xPath is loaded
-    await page.waitForSelector("//*[@id='test-p']")
+        //waits for the element with the xPath is loaded
+        await page.waitForSelector("//*[@id='test-p']")
 
-    //gets the text within that element with the specified xPath
-    const text = await page.$eval("//*[@id='test-p']", (el) => el.textContent)
+        //gets the text within that element with the specified xPath
+        const text = await page.$eval("//*[@id='test-p']", (el) => el.textContent)
 
-    //Do an expect test for that text
-    expect(text).toBe('Welcome to your Electron application.')
-    
-    //Sleep for 5secs so we can see the page 
-    await page.waitForTimeout(5000);
+        //Do an expect test for that text
+        expect(text).toBe('Welcome to your Electron application.')
+        
+        //Sleep for 5secs so we can see the page 
+        await page.waitForTimeout(5000);
 
-    // Exit app.
-    // await electronApp.close();
-
-
-})
-
-test('checks for "Welcome to your Electron application." text', async () => {
-    //waits for the element with the xPath is loaded
-    await page.waitForSelector("//*[@id='test-p']")
-    //gets the text within that element with the specified xPath
-    const text = await page.$eval("//*[@id='test-p']", (el) => el.textContent)
-
-    //Do an expect test for that text
-    //expect(text).toBe('Hello World!')
-    expect(text).toBe('Welcome')
-
-    //Sleep for 5secs so we can see the page 
-    await page.waitForTimeout(5000);
-    
-
-})
-
-test("Check if 1 + 2 equals 3", async () =>{
-    //     await page.fill("//*[@id='input-email']", "chansuck@ualberta.ca")
-//     await page.fill("//*[@id='input-password']", "password")
-//     await page.click("input[value='Login']");
-
-    //Fill in numbers and click "Add numbers" button
-    await page.fill("//*[@id='first-number']","1")
-    await page.fill("//*[@id='second-number']","2")
-    await page.click("//*[@id='add']")
-
-    const sumOfNumbers = await page.$eval("//*[@id='answer']", (el) => el.textContent)
-    expect(sumOfNumbers).toBe('3')
-    
-    //Sleep for 5secs so we can see the page 
-    await page.waitForTimeout(5000);
-
-    // Exit app.
-    // await electronApp.close();
+        // Exit app.
+        // await electronApp.close();
 
 
-})
+    })
 
-test('find 3', async () => {
-    //waits for the element with the xPath is loaded
-    await page.waitForSelector("//*[@id='answer']")
-    //gets the text within that element with the specified xPath
-    const text = await page.$eval("//*[@id='answer']", (el) => el.textContent)
+    test('checks for "Welcome to your Electron application." text', async () => {
 
-    //Do an expect test for that text
-    //expect(text).toBe('Hello World!')
-    expect(text).toBe('3')
+        // lets PlayWright know this test should fail
+        test.fail();
 
-    //Sleep for 5secs so we can see the page 
-    await page.waitForTimeout(5000);
-    
+        //waits for the element with the xPath is loaded
+        await page.waitForSelector("//*[@id='test-p']")
+        //gets the text within that element with the specified xPath
+        const text = await page.$eval("//*[@id='test-p']", (el) => el.textContent)
 
-})
+        //Do an expect test for that text
+        //expect(text).toBe('Hello World!')
+        expect(text).toBe('Welcome')
+
+        //Sleep for 5secs so we can see the page 
+        await page.waitForTimeout(5000);
+        
+
+    })
+
+    test("Check if 1 + 2 equals 3", async () =>{
+        //     await page.fill("//*[@id='input-email']", "chansuck@ualberta.ca")
+    //     await page.fill("//*[@id='input-password']", "password")
+    //     await page.click("input[value='Login']");
+
+        //Fill in numbers and click "Add numbers" button
+        await page.fill("//*[@id='first-number']","1")
+        await page.fill("//*[@id='second-number']","2")
+        await page.click("//*[@id='add']")
+
+        const sumOfNumbers = await page.$eval("//*[@id='answer']", (el) => el.textContent)
+        expect(sumOfNumbers).toBe('3')
+        
+        //Sleep for 5secs so we can see the page 
+        await page.waitForTimeout(5000);
+
+        // Exit app.
+        // await electronApp.close();
+
+
+    })
+
+    test('find 3', async () => {
+        //waits for the element with the xPath is loaded
+        await page.waitForSelector("//*[@id='answer']")
+        //gets the text within that element with the specified xPath
+        const text = await page.$eval("//*[@id='answer']", (el) => el.textContent)
+
+        //Do an expect test for that text
+        //expect(text).toBe('Hello World!')
+        expect(text).toBe('3')
+
+        //Sleep for 5secs so we can see the page 
+        await page.waitForTimeout(5000);
+        
+
+    })
 
 });
 
