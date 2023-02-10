fffcc9dce1c444734b469df4f19f98a5ae216d09
hirschfeldnic <hirschfeld.nic@gmail.com>
2023-02-09 18:00:58 -0700

    Update myApp.steps.ts

--- my-electron-app/steps/myApp.steps.ts
+++ my-electron-app/steps/myApp.steps.ts
@@ -37,7 +37,6 @@ Given('a user has inputs their password {string}', async (expectedText) =>  {
     //await page.getByPlaceholder("password").fill(expectedText)
    await page.waitForTimeout(1000);
 });
-//test
 // ? When the user clicks login button
 // Undefined. Implement with the following snippet:
 
